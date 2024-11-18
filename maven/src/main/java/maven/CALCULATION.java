92b385707da1fbbb88cafb09c7ae1dbc24fb526c
934978 <934978@LTIN123456.cts.com>
2024-11-18 18:35:11 +0530

    re change

---
+++ maven/src/main/java/maven/CALCULATION.java
@@ -0,0 +1,26 @@
+package maven;
+
+public class CALCULATION {
+
+	
+		// TODO Auto-generated method stub
+		int a;
+		int b;
+		int sum()
+		{
+			return (a+b);		
+			}
+		
+			public static void main(String[] args) 
+			{
+				CALCULATION c1= new CALCULATION();
+						c1.a=100;
+						c1.b=300;
+						int r=c1.sum();
+						System.out.println(r);
+			
+				
+		}
+	
+
+}
